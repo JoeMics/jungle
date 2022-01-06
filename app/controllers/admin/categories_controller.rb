9a82bf1e56154d1658358755a20909a4039d3b39
Joseph Micla <josephmicla@gmail.com>
2022-01-06 12:23:38 -0800

    redirects to categories list on successful subsmission

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,14 +13,14 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :products], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
   end
 
   private
-  
+
     def category_params
       params.require(:category).permit(:name)
     end
