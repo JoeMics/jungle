0ac7ed5b028aedcc9cb870d907c3fb95e328b9dc
Joseph Micla <josephmicla@gmail.com>
2022-01-06 12:39:15 -0800

    remove category list from admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,5 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.all.count()
     @category_count = Category.all.count()
-    @categories = Category.all
   end
 end
