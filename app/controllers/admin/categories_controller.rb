a76ccd5d8acf099f24202f6bf01c21d031b2ab1b
Joseph Micla <josephmicla@gmail.com>
2022-01-06 10:42:22 -0800

    creates category controller based on admin/products

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,21 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+
   def index
-    
+    @categories = Category.order(id: :desc).all
   end
 
   def new
-
+    @category = Category.new
   end
-  
+
   def create
-    
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :products], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 end
