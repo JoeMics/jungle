c85c8570d16bbdb70d8d9e0f43ab0bbeee7e953f
Joseph Micla <josephmicla@gmail.com>
2022-01-10 09:29:34 -0800

    creates specs for product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,23 +3,46 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'should save successfully when not missing name, price, quantity, and category' do
-      
+      @category = Category.new(name: 'test')
+
+      @product = Product.new(name: 'Pen', price: 100, quantity: 10, category: @category)
+      @product.save!
+
+      expect(@product.id).to be_present
     end
     
     it 'should not save when missing a name'do
-      
+      @category = Category.new(name: 'test')
+
+      @product = Product.new(name: nil, price: 100, quantity: 10, category: @category)
+      @product.save
+
+      expect(@product.id).not_to be_present
     end
-    
+
     it 'should not save when missing a price'do
-      
+      @category = Category.new(name: 'test')
+
+      @product = Product.new(name: 'Pen', price: nil, quantity: 10, category: @category)
+      @product.save
+
+      expect(@product.id).not_to be_present
     end
 
     it 'should not save when missing a quantity'do
-      
+      @category = Category.new(name: 'test')
+
+      @product = Product.new(name: 'Pen', price: 100, quantity: nil, category: @category)
+      @product.save
+
+      expect(@product.id).not_to be_present
     end
 
     it 'should not save when missing a category'do
-      
+      @product = Product.new(name: 'Pen', price: 100, quantity: 10, category: nil)
+      @product.save
+
+      expect(@product.id).not_to be_present
     end
   end
 end
