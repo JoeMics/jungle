ca59374f2eca820024fc90279143adbac6f84e97
Joseph Micla <josephmicla@gmail.com>
2022-01-10 08:44:05 -0800

    creates 'it' statements for product model spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should save successfully when not missing name, price, quantity, and category' do
+      
+    end
+    
+    it 'should not save when missing a name'do
+      
+    end
+    
+    it 'should not save when missing a price'do
+      
+    end
+
+    it 'should not save when missing a quantity'do
+      
+    end
+
+    it 'should not save when missing a category'do
+      
+    end
+  end
+end
