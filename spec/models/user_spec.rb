17c35cda6e1881face509378425fd388b62f7a92
Joseph Micla <josephmicla@gmail.com>
2022-01-10 13:04:31 -0800

    creates specs for user validations, validates email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,64 @@ RSpec.describe User, type: :model do
   
       expect(@user.id).to be_present
     end
+
+    it 'should not save successfully when first_name is missing' do
+      @user_params[:first_name] = nil
+      @user = User.new(@user_params)
+      @user.save
+  
+      expect(@user.id).to_not be_present
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'should not save successfully when last_name is missing' do
+      @user_params[:last_name] = nil
+      @user = User.new(@user_params)
+      @user.save
+  
+      expect(@user.id).to_not be_present
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it 'should not save successfully when email is missing' do
+      @user_params[:email] = nil
+      @user = User.new(@user_params)
+      @user.save
+  
+      expect(@user.id).to_not be_present
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'should not save successfully when email exists' do
+      @user1 = User.new(@user_params)
+      @user1.save
+      
+      # make sure the check is not case sensitive
+      @user_params[:email] = 'jOjO@jOmaIl.CoM'
+      @user2 = User.new(@user_params)
+      @user2.save
+  
+      expect(@user2.id).to_not be_present
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+ 
+    it 'should not save successfully when password is missing' do
+      @user_params[:password] = nil
+      @user = User.new(@user_params)
+      @user.save
+  
+      expect(@user.id).to_not be_present
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'should not save successfully when password confirmation does not match' do
+      @user_params[:password_confirmation] = 'DIO BRANDO'
+      @user = User.new(@user_params)
+      @user.save
+  
+      expect(@user.id).to_not be_present
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
   end
+  
 end
